97f0753b18ba51147c7f20f48402dc92bf251cf5
Kirti Shrestha <keertistha@gmail.com>
2022-08-23 21:14:54 +0545

    draft1

---
+++ quiz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+// Define Question Structure
+typedef struct question
+{
+    // Question Details
+    char player_name;
+    float scores;
+    int count;
+    char question;
+    char answers;
+} 
+que;
+
+
+// Save Question
+void save_question(void)
+{
+    // Ask Question Details
+    // Save to file
+    
+}
+
+void answers(char que[])
+{
+    //save answers
+    //save hint answers
+    //save to file
+}
+
+void score(char ans[], char que[])
+{
+    //highscore
+    //passing
+    //failing
+}
+
+
+
+
+int main(void)
+{
+    
+}
